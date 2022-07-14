d34fcf4d18f3f9331681d40ed359a60ddfd6b512
Phillip Witkin <pwitkin273@gmail.com>
2022-07-13 22:15:22 -0400

    added javascript to clear form client-side

--- articles/static/js/script.js
+++ articles/static/js/script.js
@@ -1,1 +1,25 @@
-console.log("script.js called")
+console.log("script.js")
+
+window.onload = init;
+
+const domElements = {
+    // Will be executed from init
+    collectDOM: function(){
+        this.clearFormButton = document.getElementById("refresh");
+        this.formText = document.getElementById("article-create-form")
+    }
+};
+
+// called when page is fully loaded
+function init(){
+    console.log("init function called")
+    domElements.collectDOM();
+    domElements.clearFormButton.addEventListener("click", clearForm);
+}
+
+// called by click event listener for 'clear' button
+function clearForm(event){
+    console.log("Form submission button clicked");
+    console.log(event.currentTarget);
+    domElements.formText.reset();
+}
--- articles/views.py
+++ articles/views.py
@@ -14,11 +14,6 @@ class ArticleListView(ListView):
     context_object_name = "article_list"
     template_name = "article_list.html"
 
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     # context['now'] = timezone.now()
-    #     return context
-
 
 class ArticleDetailView(DetailView):
     model = Article
@@ -41,8 +36,8 @@ class ArticleCreateView(CreateView):
             article = Article(
                 title=form.cleaned_data['title'],
                 body_text=form.cleaned_data['body_text'],
-                # author_id=a
             )
+            # associate it with the author and persist to the database
             article.author = author
             article.save()
             return HttpResponseRedirect('/')
